b646955d3d3737f0c7a15e4383033f5817f7b8e3
unknown <njorogekamau63@gmail.com>
2025-05-08 18:32:56 +0300

    I expanded the school url to include the related vehicles, drivers, and routes

--- transport_mgmt_api/schools/serializers.py
+++ transport_mgmt_api/schools/serializers.py
@@ -1,9 +1,15 @@
-# schools/serializers.py
 from rest_framework import serializers
 from .models import School
+from vehicles.serializers import VehicleSerializer
+from drivers.serializers import DriverSerializer
+from routes.serializers import RouteSerializer
 
 class SchoolSerializer(serializers.ModelSerializer):
+    vehicles = VehicleSerializer(many=True, read_only=True)  # Include vehicles as a nested field
+    drivers = DriverSerializer(many=True, read_only=True)    # Include drivers as a nested field
+    routes = RouteSerializer(many=True, read_only=True)      # Include routes as a nested field
+
     class Meta:
         model = School
-        fields = '__all__'
+        fields = ['id', 'name', 'address', 'phone_number', 'email', 'contact_person', 'school_admin_id', 'created_at', 'updated_at', 'vehicles', 'drivers', 'routes']
         read_only_fields = ['id', 'created_at', 'updated_at']
