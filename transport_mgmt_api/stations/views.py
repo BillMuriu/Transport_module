ad9264856c92fded524cba726c86d44ce07dab19
unknown <njorogekamau63@gmail.com>
2025-06-05 02:28:08 +0300

    This is a commit

--- transport_mgmt_api/stations/views.py
+++ transport_mgmt_api/stations/views.py
@@ -3,8 +3,16 @@ from .models import Station
 from .serializers import StationSerializer
 
 class StationListCreateView(generics.ListCreateAPIView):
-    queryset = Station.objects.all()
     serializer_class = StationSerializer
+
+    def get_queryset(self):
+        queryset = Station.objects.all()
+        route_id = self.request.query_params.get('route_id')
+
+        if route_id is not None:
+            queryset = queryset.filter(route_id=route_id)
+
+        return queryset
 
 class StationRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Station.objects.all()
--- transport_mgmt_api/trips/views.py
+++ transport_mgmt_api/trips/views.py
@@ -1,8 +1,11 @@
 from rest_framework import generics
 from rest_framework.response import Response
-
 from .models import Trip
 from .serializers import TripSerializer
+from django.utils.dateparse import parse_date
+from datetime import timedelta, datetime
+
+
 
 class NoPagination:
     display_page_controls = False
@@ -14,9 +17,24 @@ class NoPagination:
         return Response(data)
 
 class TripListCreateView(generics.ListCreateAPIView):
-    queryset = Trip.objects.all()
     serializer_class = TripSerializer
     pagination_class = NoPagination
+
+    def get_queryset(self):
+        queryset = Trip.objects.all()
+        trip_teacher = self.request.query_params.get("trip_teacher")
+        start_date = self.request.query_params.get("start_date")
+        end_date = self.request.query_params.get("end_date")
+
+        if trip_teacher:
+            queryset = queryset.filter(trip_teacher__id=trip_teacher)
+        
+        if start_date:
+            queryset = queryset.filter(created_at__date__gte=parse_date(start_date))
+        if end_date:
+            queryset = queryset.filter(created_at__date__lte=parse_date(end_date))
+
+        return queryset
 
 class TripRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Trip.objects.all()
