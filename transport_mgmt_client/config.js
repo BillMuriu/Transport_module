3d39e2cf80bfbc99a73b44df3faa5ea11495139d
unknown <njorogekamau63@gmail.com>
2025-06-16 19:28:05 +0300

    The firdt version of the super admin section

--- transport_mgmt_client/config.js
+++ transport_mgmt_client/config.js
@@ -1,3 +1,3 @@
 // config.js
-// export const API_BASE_URL = "http://localhost:8000";
-export const API_BASE_URL = "https://transport-module.onrender.com";
+export const API_BASE_URL = "http://localhost:8000";
+// export const API_BASE_URL = "https://transport-module.onrender.com";
